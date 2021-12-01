39603a77b3aeaaada4ae441bea627b43059d5540
Sean Kermes <skermes@gmail.com>
2021-12-01 07:26:38 -0500

    Solve Day 1

--- src/days/one.rs
+++ src/days/one.rs
@@ -1,11 +1,38 @@
+use std::num::ParseIntError;
 use crate::aoc_error::AocError;
 
-pub const NAME: &str = "Unknown";
+pub const NAME: &str = "Sonar Sweep";
 
-pub fn part_one(_input: &str) -> Result<String, AocError> {
-    Ok("Not implemented".to_string())
+pub fn part_one(input: &str) -> Result<String, AocError> {
+    let nums = input
+        .lines()
+        .map(|l| l.parse())
+        .collect::<Result<Vec<usize>, ParseIntError>>()?;
+
+    let steps_up = nums.iter()
+        .zip(nums.iter().skip(1))
+        .filter(|(prev, next)| prev < next)
+        .count();
+
+    Ok(steps_up.to_string())
 }
 
-pub fn part_two(_input: &str) -> Result<String, AocError> {
-    Ok("Not implemented".to_string())
+pub fn part_two(input: &str) -> Result<String, AocError> {
+    let nums = input
+        .lines()
+        .map(|l| l.parse())
+        .collect::<Result<Vec<usize>, ParseIntError>>()?;
+
+    let window_sums: Vec<usize> = nums.iter()
+        .zip(nums.iter().skip(1))
+        .zip(nums.iter().skip(2))
+        .map(|((x, y), z)| x + y + z)
+        .collect();
+
+    let steps_up = window_sums.iter()
+        .zip(window_sums.iter().skip(1))
+        .filter(|(prev, next)| prev < next)
+        .count();
+
+    Ok(steps_up.to_string())
 }
