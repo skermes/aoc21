0d1b374bcfe9e4dbc7779bbae7b06c942f5f079b
Sean Kermes <skermes@gmail.com>
2021-12-05 10:11:21 -0500

    Solve day 5
    There's something odd going on with the performance of this day,
    where whichever part I run first takes an extra ~2ms. Not sure
    what's going on, but I don't feel like diving deep enough to
    figure it out.

--- src/days/five.rs
+++ src/days/five.rs
@@ -1,11 +1,165 @@
+use std::str::FromStr;
 use crate::aoc_error::AocError;
 
-pub const NAME: &str = "Unknown";
+pub const NAME: &str = "Hydrothermal Venture";
 
-pub fn part_one(_input: &str) -> Result<String, AocError> {
-    Ok("Not implemented".to_string())
+fn semisign(x: isize) -> isize {
+    if x < 0 {
+        -1
+    } else if x > 0 {
+        1
+    } else {
+        0
+    }
 }
 
-pub fn part_two(_input: &str) -> Result<String, AocError> {
-    Ok("Not implemented".to_string())
+#[derive(Debug, Copy, Clone)]
+struct Point(isize, isize);
+
+impl FromStr for Point {
+    type Err = AocError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (x, y) = s.split_once(",").ok_or_else(|| AocError::Misc(format!("Invalid point \"{}\"", s)))?;
+        Ok(Point(x.parse()?, y.parse()?))
+    }
+}
+
+impl Point {
+    fn x(&self) -> isize { self.0 }
+    fn y(&self) -> isize { self.1 }
+}
+
+#[derive(Debug, Copy, Clone)]
+struct Line {
+    from: Point,
+    to: Point
+}
+
+impl FromStr for Line {
+    type Err = AocError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (from, to) = s.split_once(" -> ").ok_or_else(|| AocError::Misc(format!("Invalid line \"{}\"", s)))?;
+        Ok(Line { from: from.parse()?, to: to.parse()? })
+    }
+}
+
+impl Line {
+    fn is_axis_aligned(&self) -> bool {
+        self.from.x() == self.to.x() || self.from.y() == self.to.y()
+    }
+
+    fn points(&self) -> LinePoints {
+        LinePoints::new(self)
+    }
+}
+
+#[derive(Debug)]
+struct LinePoints {
+    x: isize,
+    y: isize,
+    dx: isize,
+    dy: isize,
+    to: Point,
+    done: bool
+}
+
+impl Iterator for LinePoints {
+    type Item = Point;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.done {
+            return None;
+        }
+
+        self.x += self.dx;
+        self.y += self.dy;
+
+        if self.x == self.to.x() && self.y == self.to.y() {
+            self.done = true;
+        }
+
+        Some(Point(self.x, self.y))
+    }
+}
+
+impl LinePoints {
+    fn new(line: &Line) -> Self {
+        // This only works for vertical, horiztonal and 45 degree lines, but
+        // thats all the problem asks for.
+        let dx = semisign(line.to.x() - line.from.x());
+        let dy = semisign(line.to.y() - line.from.y());
+
+        LinePoints {
+            dx, dy,
+            // Subtract one tick here so that we can add it back during the
+            // first iteration.
+            x: line.from.x() - dx,
+            y: line.from.y() - dy,
+            to: line.to,
+            done: false
+        }
+    }
+}
+
+#[derive(Debug)]
+struct VentField2 {
+    counts: [[u16; 1000]; 1000],
+    overlaps: usize
+}
+
+impl VentField2 {
+    fn add(&mut self, point: Point) {
+        self.counts[point.x() as usize][point.y() as usize] += 1;
+
+        if self.counts[point.x() as usize][point.y() as usize] == 2 {
+            self.overlaps += 1;
+        }
+    }
+
+    fn new() -> Self {
+        VentField2 {
+            counts: [[0; 1000]; 1000],
+            overlaps: 0
+        }
+    }
+
+    fn overlaps(&self) -> usize {
+        self.overlaps
+    }
+}
+
+pub fn part_one(input: &str) -> Result<String, AocError> {
+    let lines = input
+        .lines()
+        .map(|l| l.parse())
+        .collect::<Result<Vec<Line>, AocError>>()?;
+    let mut field = VentField2::new();
+
+    for line in lines {
+        if line.is_axis_aligned() {
+            for point in line.points() {
+                field.add(point);
+            }
+        }
+    }
+
+    Ok(field.overlaps().to_string())
+}
+
+pub fn part_two(input: &str) -> Result<String, AocError> {
+    let lines = input
+        .lines()
+        .map(|l| l.parse())
+        .collect::<Result<Vec<Line>, AocError>>()?;
+    let mut field = VentField2::new();
+
+    for line in lines {
+        for point in line.points() {
+            field.add(point);
+        }
+    }
+
+    Ok(field.overlaps().to_string())
 }
--- src/days/four.rs
+++ src/days/four.rs
@@ -48,6 +48,19 @@ impl FromStr for Board {
     }
 }
 
+const SLICES: [[usize; 5]; 10] = [
+    [0, 1, 2, 3, 4],
+    [5, 6, 7, 8, 9],
+    [10, 11, 12, 13, 14],
+    [15, 16, 17, 18, 19],
+    [20, 21, 22, 23, 24],
+    [0, 5, 10, 15, 20],
+    [1, 6, 11, 16, 21],
+    [2, 7, 12, 17, 22],
+    [3, 8, 13, 18, 23],
+    [4, 9, 14, 19, 24]
+];
+
 impl Board {
     fn mark(&mut self, number: usize) {
         for n in self.numbers.iter_mut() {
@@ -61,19 +74,7 @@ impl Board {
     }
 
     fn has_won(&self) -> bool {
-        let slices = [
-            [0, 1, 2, 3, 4],
-            [5, 6, 7, 8, 9],
-            [10, 11, 12, 13, 14],
-            [15, 16, 17, 18, 19],
-            [20, 21, 22, 23, 24],
-            [0, 5, 10, 15, 20],
-            [1, 6, 11, 16, 21],
-            [2, 7, 12, 17, 22],
-            [3, 8, 13, 18, 23],
-            [4, 9, 14, 19, 24]
-        ];
-        slices.iter()
+        SLICES.iter()
             .map(|slice| slice.iter()
                             .map(|i| self.numbers[*i])
                             .all(|n| n.is_match()))
