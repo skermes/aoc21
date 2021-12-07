8dbb85f548c517667560d9fa5f14c764011ce8f7
Sean Kermes <skermes@gmail.com>
2021-12-07 07:43:38 -0500

    Solve day 7

--- src/days/seven.rs
+++ src/days/seven.rs
@@ -1,11 +1,65 @@
+use std::num::ParseIntError;
 use crate::aoc_error::AocError;
 
-pub const NAME: &str = "Unknown";
+pub const NAME: &str = "The Treachery of Whales";
 
-pub fn part_one(_input: &str) -> Result<String, AocError> {
-    Ok("Not implemented".to_string())
+// Like regular median except it returns the higher of the middle pair when the
+// list has an even number of items.
+// Input must be sorted.
+fn crab_median(positions: &[usize]) -> usize {
+    positions[positions.len() / 2]
 }
 
-pub fn part_two(_input: &str) -> Result<String, AocError> {
-    Ok("Not implemented".to_string())
+// Like regular mean but truncates the division instead of using floats or
+// rounding properly.
+fn crab_mean(positions: &[usize]) -> usize {
+    let sum: usize = positions.iter().sum();
+    sum / positions.len()
+}
+
+fn fuel_cost_p1(position: usize, target: usize) -> usize {
+    if position < target {
+        target - position
+    } else {
+        position - target
+    }
+}
+
+// Fuel costs in part 2 are triangle numbers
+fn fuel_cost_p2(position: usize, target: usize) -> usize {
+    let distance = fuel_cost_p1(position, target);
+    (distance * (distance + 1)) / 2
+}
+
+pub fn part_one(input: &str) -> Result<String, AocError> {
+    let mut positions = input
+        .split(",")
+        .map(|s| s.parse())
+        .collect::<Result<Vec<usize>, ParseIntError>>()?;
+    positions.sort();
+
+    let best_gather_spot = crab_median(&positions);
+
+    let total_fuel: usize = positions
+        .iter()
+        .map(|p| fuel_cost_p1(*p, best_gather_spot))
+        .sum();
+
+    Ok(total_fuel.to_string())
+}
+
+pub fn part_two(input: &str) -> Result<String, AocError> {
+    let positions = input
+        .split(",")
+        .map(|s| s.parse())
+        .collect::<Result<Vec<usize>, ParseIntError>>()?;
+
+    let best_gather_spot = crab_mean(&positions);
+
+    let total_fuel: usize = positions
+        .iter()
+        .map(|p| fuel_cost_p2(*p, best_gather_spot))
+        .sum();
+
+    Ok(total_fuel.to_string())
 }
